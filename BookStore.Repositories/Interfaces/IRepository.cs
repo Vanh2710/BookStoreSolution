dc775ed25fa8c0dfdb7da16941f4946e4ed3d8fd
Việt Anh <nguyenvietpro6@gmail.com>
2026-06-19 16:45:10 +0700

    feat: implement generic repository pattern in Repositories layer

---
+++ BookStore.Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookStore.Repositories
+{
+  public interface IRepository<T> where T : class
+  {
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T?> GetByIdAysnc(int id);
+    Task AddAsync(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+    Task<bool> SaveChangesAsync();
+  }
+}
